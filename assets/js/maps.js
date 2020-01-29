001acc7c06553e34cd3ec22da54b5b8c1dcd693a
amosborn <amosborn@mail.com>
2020-01-29 22:23:59 +0000

    initial commit

---
+++ assets/js/maps.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 5,
+        center: {
+            lat: -28.7282,
+            lng: 24.7499
+        }
+    });
+}
