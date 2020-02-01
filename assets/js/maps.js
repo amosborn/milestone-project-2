637bfec8b478c7c23a4dbe727ad21590d138fe76
amosborn <amosborn@mail.com>
2020-02-01 20:42:35 +0000

    added autocomplete to maps.js and radio buttons for search types

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,9 +1,35 @@
+var map, places;
+var markers = [];
+var autocomplete;
+var countryRestrict = {'country': 'za'};
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 5,
         center: {
             lat: -28.7282,
             lng: 24.7499
         }
     });
+
+autocomplete = new google.maps.places.Autocomplete((
+    document.getElementById('autocomplete')), {
+        types: ['(cities)'],
+        componentRestrictions: countryRestrict
+    });
+
+    places = new google.maps.places.PlacesService(map);
+
+    autocomplete.addListener('place_changed', onPlaceChanged);
 }
+
+function onPlaceChanged() {
+    var place = autocomplete.getPlace();
+    if (place.geometry) {
+        map.panTo(place.geometry.location);
+        map.setZoom(14);
+    } else {
+        document.getElementById('autocomplete').placeholder = 'Enter a city';
+    }
+}
+
